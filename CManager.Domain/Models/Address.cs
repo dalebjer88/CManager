19f1a422933d6344f87d9bc90a213e7a48a19b69
Mattias <dalebjer88@gmail.com>
2025-12-11 13:09:21 +0100

    feat: add Customer and Address models

---
+++ CManager.Domain/Models/Address.cs
@@ -0,0 +1,7 @@
+﻿namespace CManager.Domain.Models;
+public class Address
+{
+    public string Street { get; set; } = string.Empty;
+    public string PostalCode { get; set; } = string.Empty;
+    public string City { get; set; } = string.Empty;
+}
---
+++ CManager.Domain/Models/Customer.cs
@@ -0,0 +1,12 @@
+﻿namespace CManager.Domain.Models;
+public class Customer
+{
+    public Guid Id { get; set; } 
+    public string FirstName { get; set; } = string.Empty;
+    public string LastName { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public string PhoneNumber { get; set; } = string.Empty;
+    public Address Address { get; set; } = new Address();
+
+    public string FullName => $"{FirstName} {LastName}";
+}
