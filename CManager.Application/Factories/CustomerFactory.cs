f0b8090e2321c2543b585a97a2bac0a0a9b94b9f
Mattias <dalebjer88@gmail.com>
2026-01-05 13:09:22 +0100

    Add CustomerFactory for creating Customer domain models

---
+++ CManager.Application/Factories/CustomerFactory.cs
@@ -0,0 +1,34 @@
+﻿using CManager.Application.Interfaces;
+using CManager.Domain.Models;
+
+namespace CManager.Application.Factories;
+
+public sealed class CustomerFactory : ICustomerFactory
+{
+    public Customer Create(
+        Guid id,
+        string firstName,
+        string lastName,
+        string email,
+        string phoneNumber,
+        string street,
+        string postalCode,
+        string city
+    )
+    {
+        return new Customer
+        {
+            Id = id,
+            FirstName = firstName,
+            LastName = lastName,
+            Email = email,
+            PhoneNumber = phoneNumber,
+            Address = new Address
+            {
+                Street = street,
+                PostalCode = postalCode,
+                City = city
+            }
+        };
+    }
+}
---
+++ CManager.Application/Interfaces/ICustomerFactory.cs
@@ -0,0 +1,17 @@
+﻿using CManager.Domain.Models;
+
+namespace CManager.Application.Interfaces;
+
+public interface ICustomerFactory
+{
+    Customer Create(
+        Guid id,
+        string firstName,
+        string lastName,
+        string email,
+        string phoneNumber,
+        string street,
+        string postalCode,
+        string city
+    );
+}
