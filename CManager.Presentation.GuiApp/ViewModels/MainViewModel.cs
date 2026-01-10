3feece5187d3ff607365fe1d44302a52f879e77f
Mattias <dalebjer88@gmail.com>
2026-01-10 09:30:59 +0100

    feat: implement dynamic view switching using DataTemplates and CurrentViewModel

--- CManager.Presentation.GuiApp/ViewModels/MainViewModel.cs
+++ CManager.Presentation.GuiApp/ViewModels/MainViewModel.cs
@@ -1,10 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 
-namespace CManager.Presentation.GuiApp.ViewModels
+namespace CManager.Presentation.GuiApp.ViewModels;
+
+public partial class MainViewModel : ObservableObject
 {
-    internal class MainViewModel
+    private readonly CustomersListViewModel _customersViewModel;
+    private readonly CreateCustomerViewModel _createViewModel;
+
+    [ObservableProperty]
+    private object? _currentViewModel;
+
+    public MainViewModel(CustomersListViewModel customersViewModel, CreateCustomerViewModel createViewModel)
     {
+        _customersViewModel = customersViewModel;
+        _createViewModel = createViewModel;
+
+        CurrentViewModel = _customersViewModel;
     }
+
+    [RelayCommand]
+    private void ShowCustomersView()
+    {
+        _customersViewModel.LoadCustomers();
+        CurrentViewModel = _customersViewModel;
+    }
+
+    [RelayCommand]
+    private void ShowCreateView() => CurrentViewModel = _createViewModel;
 }
