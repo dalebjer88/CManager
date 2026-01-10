07cb5e44efa13effbedfb21cdee00afc5fe6a80f
Mattias <dalebjer88@gmail.com>
2026-01-10 09:25:07 +0100

    refactor: move data storage to LocalApplicationData for cross-app synchronization

--- CManager.Presentation.GuiApp/App.xaml.cs
+++ CManager.Presentation.GuiApp/App.xaml.cs
@@ -1,28 +1,57 @@
-﻿using System.Windows;
-using Microsoft.Extensions.DependencyInjection;
+﻿using CManager.Application.Factories;
+using CManager.Application.Helpers;
+using CManager.Application.Interfaces;
+using CManager.Application.Services;
+using CManager.Domain.Interfaces;
+using CManager.Infrastructure.Formatters;
+using CManager.Infrastructure.Repos;
 using CManager.Presentation.GuiApp.ViewModels;
+using Microsoft.Extensions.DependencyInjection;
+using System.IO;
+using System.Windows;
 
 namespace CManager.Presentation.GuiApp;
 
 public partial class App : System.Windows.Application
 {
-    private ServiceProvider? _serviceProvider;
+    private readonly IServiceProvider _serviceProvider;
+
+    public App()
+    {
+        var services = new ServiceCollection();
+
+        var folderPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "CManager");
+        var filePath = Path.Combine(folderPath, "customers.json");
+
+        if (!Directory.Exists(folderPath))
+        {
+            Directory.CreateDirectory(folderPath);
+        }
+
+        services.AddSingleton<IIdGenerator, IdGenerator>();
+        services.AddSingleton<ICustomerFactory, CustomerFactory>();
+        services.AddSingleton<IJsonFormatter, JsonFormatter>();
+
+        services.AddSingleton<ICustomerRepo>(sp =>
+            new CustomerRepo(filePath, sp.GetRequiredService<IJsonFormatter>()));
+
+        services.AddSingleton<ICustomerService, CustomerService>();
+        services.AddSingleton<MainViewModel>();
+        services.AddSingleton<CustomersListViewModel>();
+        services.AddSingleton<CreateCustomerViewModel>();
+
+        services.AddSingleton<MainWindow>(sp => new MainWindow
+        {
+            DataContext = sp.GetRequiredService<MainViewModel>()
+        });
+
+        _serviceProvider = services.BuildServiceProvider();
+    }
 
     protected override void OnStartup(StartupEventArgs e)
     {
-        base.OnStartup(e);
-
-        var services = new ServiceCollection();
-
-        services.AddTransient<MainViewModel>();
-
-        _serviceProvider = services.BuildServiceProvider();
-
-        var mainWindow = new MainWindow
-        {
-            DataContext = _serviceProvider.GetRequiredService<MainViewModel>()
-        };
-
+        var mainWindow = _serviceProvider.GetRequiredService<MainWindow>();
         mainWindow.Show();
+        base.OnStartup(e);
     }
 }
